0554eff42ccdd853c33d96c2d9d6b5a3dddd2078
Robel Tirfe <robel@gmail.com>
2019-01-02 15:16:07 -0500

    First Commit.

---
+++ app/src/main/java/com/example/robel/xposedmodule/XposedClass.java
@@ -0,0 +1,30 @@
+package com.example.robel.xposedmodule;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class XposedClass implements IXposedHookLoadPackage {
+
+
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        String classToHook = "com.example.robel.TestIdeaVim.MainActivity";
+        String functionToHook = "setOutput";
+
+        if(lpparam.packageName.equals("com.example.robel.TestIdeaVim")){
+            XposedBridge.log("Loaded app: " + lpparam.packageName);
+            findAndHookMethod(classToHook,lpparam.classLoader, functionToHook, int.class,
+                    new XC_MethodHook(){
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable{
+                            param.args[0] = 1;
+                            XposedBridge.log("value of i after hooking" +  param.args[0]);
+                        }
+                    });
+        }
+
+    }
+}
