0f174af921202cfb6cf1724df7729c2fec3c5934
Robel <robel.tirfe@gmail.com>
2019-01-25 18:43:48 -0500

    A clean app that can be used as a reference on how to hook on to a method.

--- app/src/main/java/com/example/robel/xposedmodule/XposedClass.java
+++ app/src/main/java/com/example/robel/xposedmodule/XposedClass.java
@@ -1,6 +1,4 @@
 package com.example.robel.xposedmodule;
-import android.graphics.Color;
-import android.widget.TextView;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,124 +10,58 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 //Get notified when an app ("Android package") is loaded.
 // This is especially useful to hook some app-specific methods.
 public class XposedClass implements IXposedHookLoadPackage {
-    //TODO android.content.ContextWrapper.getApplicationContext();
-    //Need to be able to isolate a network flow.
 
     //String array to hold our top 100 android apps class path.
-    private String appsToHook []= {
-            "com.example.robel.testideavim",
-            "com.google.android.youtube",
-            "com.facebook.orca",
-            "com.king.candycrushsaga",
-            "com.twitter.android",
-            "android.content.ContextWrapper.getApplicationContext()",
-            "com.android.inputmethod.keyboard"};
+    private String appsToHook []= {"com.example.robel.testideavim"};
 
-    //handleLoadPackage
-    //This method is called when an app is loaded. It's called very early, even before Application.onCreate() is called.
-    //Modules can set up their app-specific hooks here.
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        XposedBridge.log("\tLoaded app: " + lpparam.packageName);
+        hookOnpackage(lpparam);
+    }
 
-        switch (lpparam.packageName){
-            case "teamtreehouse.com.iamhere":
-                XposedBridge.log("Hooking on getApplicationContext");
-                break;
-            case "android.content.ContextWrapper.getApplicationContext()":
-                XposedBridge.log("Hooking on getApplicationContext");
-                break;
-            case "com.android.inputmethod.keyboard":
-                XposedBridge.log("Hooking on keyboard");
-                break;
-            case "com.example.robel.testideavim":
-                XposedBridge.log("Hooking on TestIdeaVim");
-                break;
-            case "com.google.android.youtube":
-                XposedBridge.log("Hooking on YouTube");
-                break;
-            case "com.facebook.orca":
-                XposedBridge.log("Hooking on FaceBook");
-                break;
-            case "com.king.candycrushsaga":
-                XposedBridge.log("Hooking on candy crush saga");
-                break;
-            case "com.twitter.android":
-                XposedBridge.log("Hooking on twitter");
-                break;
-            default:
-                break;
-        }
-
-        String classToHook = "com.example.robel.testideavim.MainActivity";
-        String functionToHook = "setOutput";
-        String functionToHook2 = "getSystemService";
-
+    public void hookOnpackage(LoadPackageParam lpparam){
         if(lpparam.packageName.equals("com.example.robel.testideavim")){
-            XposedBridge.log("Loaded app: " + lpparam.packageName);
-
-//            findAndHookMethod(classToHook,lpparam.classLoader, functionToHook, int.class, new XC_MethodHook(){
-            findAndHookMethod(classToHook,lpparam.classLoader, functionToHook, int.class, new XC_MethodHook(){
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable{
-                    param.args[0] = 1;
-                    XposedBridge.log("value of i after hooking" +  param.args[0]);
-                }
-            });
-//            findAndHookMethod(classToHook, lpparam.classLoader, functionToHook2, String.class, new XC_MethodHook() {
-//                @Override
-//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                    XposedBridge.log("Hooking to network context");
-//                    XposedBridge.log("Value of first param: " +  param.args[0]);
-//                }
-//
-//            });
-
-            findAndHookMethod("android.net.ConnectivityManager", lpparam.classLoader, "getActiveNetworkInfo", new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("%nInside android.net.ConnectivityManager");
-                }
-            });
-            findAndHookMethod("android.net.NetworkInfo", lpparam.classLoader, "getTypeName", new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("%nInside android.net.NetworkInfo");
-                }
-            });
-            findAndHookMethod("com.google.android.gms.location.LocationServices", lpparam.classLoader, "getLastLocation", new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("/ncom.google.android.gms.location.getLastLocation");
-                }
-            });
-            findAndHookMethod("com.google.android.gms.location.LocationServices", lpparam.classLoader, "LocationServices", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            XposedBridge.log("/ncom.google.android.gms.location.LocationServices");
-                        }
-                    });
-//            com.google.android.gms.location.LocationServices.LocationServices()
-//            com.google.android.gms.location.LocationServices.FusedLocationProviderApi.getLastLocation
-//            com.google.android.gms.location.FusedLocationProviderApi.getLastLocation
-//            com.google.android.gms.location.FusedLocationProviderApi
-//            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-            findAndHookMethod("android.systemui.statusbar.policy.Clock", lpparam.classLoader, "getLastKnownLocation",String.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("%nInside com.android.systemui.statusbar.policy.Clock");
-                    TextView tv = (TextView) param.thisObject;
-                    String text = tv.getText().toString();
-                    tv.setText(text + " :)");
-                    tv.setTextColor(Color.RED);
-                }
-            });
+            hookOnDummyApp(lpparam);
+            hookOnNetworkInfo(lpparam);
+            hookGetActiveNetworkInfo(lpparam);
         }
-//        if(lpparam.packageName.equals(classToHook2)){
-//            XposedBridge.log("Hooking on YouTube");
-//        }
-//
+
+        if(lpparam.packageName.equals("teamtreehouse.com.iamhere")){
+            //TODO create a hooking for getLocation here
+        }
 
     }
+
+    private void hookOnDummyApp(LoadPackageParam lpparam) {
+        findAndHookMethod(lpparam.packageName + ".MainActivity", lpparam.classLoader, "setOutput", int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                param.args[0] = 1;
+                XposedBridge.log("value of i after hooking" +  param.args[0]);
+            }
+        });
+    }
+
+    private void hookOnNetworkInfo(final LoadPackageParam lpparam){
+        findAndHookMethod("android.net.NetworkInfo", lpparam.classLoader, "getTypeName", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("\tInside android.net.NetworkInfo <- called by "+lpparam.packageName);
+            }
+        });
+    }
+
+    private void hookGetActiveNetworkInfo(final LoadPackageParam lpparam) {
+
+        findAndHookMethod("android.net.ConnectivityManager", lpparam.classLoader, "getActiveNetworkInfo", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("\tInside android.net.ConnectivityManager <- called by "+ lpparam.packageName);
+            }
+        });
+    }
+
 
 }
